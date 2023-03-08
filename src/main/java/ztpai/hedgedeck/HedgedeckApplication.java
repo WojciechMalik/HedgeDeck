47116d05be79051dcbd4042533fdc53d327134e5
Lotharito <pisarzblog@gmail.com>
2023-03-08 11:38:43 +0100

    basic skeleton of project1

---
+++ src/main/java/ztpai/hedgedeck/HedgedeckApplication.java
@@ -0,0 +1,13 @@
+package ztpai.hedgedeck;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HedgedeckApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HedgedeckApplication.class, args);
+	}
+
+}
