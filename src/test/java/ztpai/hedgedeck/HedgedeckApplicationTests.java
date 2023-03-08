64cd604bc8e97af435608458c9170c9c20a648ed
Lotharito <pisarzblog@gmail.com>
2023-03-08 11:38:43 +0100

    basic skeleton of project1

---
+++ src/test/java/ztpai/hedgedeck/HedgedeckApplicationTests.java
@@ -0,0 +1,13 @@
+package ztpai.hedgedeck;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HedgedeckApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
