7b213237da3b86f74e9b9174320fea4c79fa0028
WojciechMalik <pisarzblog@gmail.com>
2023-05-27 23:04:46 +0200

    front app

--- front/src/App.js
+++ front/src/App.js
@@ -1,24 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+import Login from "./views/login";
+import Dashboard from "./views/dashboard";
+import NewSet from "./views/newset";
+import Registration from "./views/registration"; 
+import Practise from "./views/practise";
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+          
+          <Route path="/login" element={<Login />} />
+          <Route path="/logout" element={<Login />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/newSet" element={<NewSet />} />
+          <Route path="/registration" element={<Registration />} />
+          <Route path="/practise" element={<Practise />} /> 
+
+    </Routes>
+  </Router>
   );
 }
 
