e24abbeb456e29ae6b8fe38f2420300e24014d20
Lotharito <wojciech.malik@student.pk.edu.pl>
2023-05-27 19:07:04 +0200

    create entities

--- src/main/java/ztpai/wojciech_malik/hedgedeck/controllers/UserDetailsController.java
+++ src/main/java/ztpai/wojciech_malik/hedgedeck/controllers/UserDetailsController.java
@@ -4,6 +4,7 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import ztpai.wojciech_malik.hedgedeck.repositories.SetRepository;
 import ztpai.wojciech_malik.hedgedeck.repositories.UserDetailsRepository;
 import ztpai.wojciech_malik.hedgedeck.repositories.UserRepository;
 
@@ -15,4 +16,6 @@ public class UserDetailsController {
     UserDetailsRepository userDetailsRepository;
     @Autowired
     UserRepository userRepository;
+    @Autowired
+    SetRepository setRepository;
 }
